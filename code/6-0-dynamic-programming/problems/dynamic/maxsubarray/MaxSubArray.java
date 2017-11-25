8e938f95040b41fd2abaff3a84957fc82a001e92
jayadeepj <jayadeepj@gmail.com>
2017-11-26 09:06:31 +1100

    Max Sub Array Sum

---
+++ code/6-0-dynamic-programming/problems/dynamic/maxsubarray/MaxSubArray.java
@@ -0,0 +1,41 @@
+package dynamic.maxsubarray;
+
+/**
+ * ind the contiguous subarray within an array (containing at least one number)
+ * which has the largest sum
+ * 
+ * @author jayadeep
+ *
+ */
+public class MaxSubArray {
+
+	public static void main(String[] args) {
+     int[] input = new int[] {-2,1,-3,4,-1,2,1,-5,4};
+     System.out.println(maxSubArray(input););
+     
+	}
+
+	/**
+	 * 
+	 * @param input
+	 * @return
+	 */
+	private static int maxSubArray(int[] input) {
+		int[] maxLengthSoFar = new int[input.length];
+		maxLengthSoFar[0] = input[0];
+		for (int i = 1; i < input.length; i++) {
+			maxLengthSoFar[i] = Math.max(input[i], maxLengthSoFar[i - 1] + input[i]);
+		}
+		int maxSum = maxLengthSoFar[findMaxIndex(maxLengthSoFar)];
+		return maxSum;
+	}
+
+	private static int findMaxIndex(int[] maxLengthSoFar) {
+		int maxIndex = 0;
+		for (int i = 1; i < maxLengthSoFar.length; i++) {
+			if (maxLengthSoFar[i] > maxLengthSoFar[maxIndex])
+				maxIndex = i;
+		}
+		return maxIndex;
+	}
+}
---
+++ code/6-0-dynamic-programming/problems/dynamic/maxsubarray/README.md
@@ -0,0 +1,24 @@
+# Max Sub Array Problem By DP
+
+
+## Problem 
+
+ Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+ For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+ the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+
+## Problem Description 
+
+## Solution
+
+ We use two max, maxSoFar -> max sub array found so far and maxEndingHere -> maximum sub array size that ends in this index. This is in place solution and time Complexity is O(N).
+
+##Dependencies 
+
+
+## Output
+
+
+
+
+##References
